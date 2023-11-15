28cd9ec165c3bde5cec9cf3a022dd0a9f8981932
kristofaumeh <xtopherumeh@gmail.com>
2023-11-15 13:41:55 +0100

    the getline function

--- getlyn.c
+++ getlyn.c
@@ -1,16 +1,24 @@
 #include "shell.h"
+/**
+ * main - Reads input from a stream.
+ * @lineptr: A buffer to store the input.
+ * @k: The size of lineptr.
+ * @getlyn: The stream to read from.
+ * Return: The number of bytes read.
+ */
 
-int main() {
+int main(char *lineptr, size_t k, ssize_t getlyn)
+{
 		char *lineptr;
 		size_t k;
 		ssize_t getlyn;
 
 		getlyn = getline(&lineptr, &k, STDIN_FILENO);
-		if (getlyn == -1) {
+		if (getlyn == -1)
+		{
 			perror("getline did fail");
 			exit(-1);
-			}
-
+		}
 		printf("%s\n", lineptr);
 
 		return (0);
