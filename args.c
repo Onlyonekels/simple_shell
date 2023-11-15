3dea0af201e51eeb544a80849546956af780afdd
kristofaumeh <xtopherumeh@gmail.com>
2023-11-15 13:43:07 +0100

    This handles command line arguement

--- args.c
+++ args.c
@@ -1,17 +1,26 @@
 #include "shell.h"
+/**
+ * main - A program that handles command line arguements
+ * @argcount: Arguement count
+ * @argvector: arguement vector
+ * Return: On success, returns the pointer to the arrar
+ */
 
-int main(int argcount, char *argvector[]) {
-	int k = 0;
-	(void)argcount;
-	while (argvector[k] != NULL) {
-		if (strcmp(argvector[k], "mkdir") == 0) {
+int main(int argcount, char *argvector[])
+{
+		int k = 0;
+		(void)argcount;
+		while (argvector[k] != NULL)
+		{
+			if (strcmp(argvector[k], "mkdir") == 0)
+			{
 
 				printf("a valid command was typed\n");
-					printf("%s\n", argvector[k]);
-		}
-		printf("argvector[%d] : %s\n", k, argvector[k]);
+				printf("%s\n", argvector[k]);
+			}
+			printf("argvector[%d] : %s\n", k, argvector[k]);
 
-		k++;
-	}
-	return (0);
+				k++;
+		}
+		return (0);
 }
